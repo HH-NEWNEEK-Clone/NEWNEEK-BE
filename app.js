5dfaf030ddb4a59c916bb14ffcc10498dc6e3579
803571 <82869298+803571@users.noreply.github.com>
2024-01-23 17:19:37 +0900

    Update app.js
    DongHyeon 브랜치에서 main 브랜치로 app.js 내용 변경

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 import express from 'express';
 import crawlingCategory from './src/routes/crawling.bycategory.js'
 import sendEmail from './src/routes/send-email.js'
+import userRouter from './src/routes/users.router.js';
+import errorHandlingMiddleware from "./src/middlewares/error.handling.middleware.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import session from 'express-session';
 
 const app = express()
 const port = 3000
 
+app.use(cors());
 app.use(express.json())
+app.use(cookieParser());
+
+
+app.use(
+    session({
+        secret: 'process.env.MY_SECRET_KEY',
+        resave: false,
+        saveUninitialized: false,
+        cookie: { secure: true }
+    })
+);
+
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -15,7 +33,9 @@ router.get("/", (req, res) => {
     return res.json({ message: "hello world!!" })
 })
 app.use("/api", [crawlingCategory, sendEmail]);
+app.use("/api", [userRouter]);
+app.use(errorHandlingMiddleware);
 
 app.listen(port, () => {
-    console.log(port, "Server Start")
+    console.log(port, "서버가 열렸습니다.")
 })
