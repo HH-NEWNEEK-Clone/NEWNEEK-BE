0e3971b03abb841375665e06a4beff3321af5ac4
song_woojin <ujins8201@gmail.com>
2024-01-24 09:48:53 +0900

    FIX

--- app.js
+++ app.js
@@ -10,19 +10,22 @@ import session from 'express-session';
 const app = express()
 const port = 3000
 
-const whitelist = ['http://localhost:3000', 'http://example2.com']
-let corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// const whitelist = ['http://localhost:3000', 'http://example2.com']
+// let corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
 
-app.use(cors(corsOptions));
+app.use(cors({
+    origin: '*',
+    // credential: ?
+}));
 
 app.use(express.json());
 app.use(cookieParser());
