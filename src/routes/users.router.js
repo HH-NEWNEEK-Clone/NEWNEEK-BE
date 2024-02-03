f6dc47805bb041abfbf6a7dbb95a6ad9c024a687
song_woojin <ujins8201@gmail.com>
2024-02-03 17:45:08 +0900

    12

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -128,14 +128,12 @@ usersRouter.get('/auth/kakao/callback', async (req, res) => {
     console.log(code)
     try {
       const tokenResponse = await axios.post('https://kauth.kakao.com/oauth/token', {
-        grant_type: 'authorization_code',
-        client_id: REST_API_KEY,
-        // client_secret: KAKAO_CLIENT_SECRET, // 이 부분을 주석 처리 또는 제거하세요
-        redirect_uri: REDIRECT_URI,
-        code
-    });
-            
-        const accessToken = tokenResponse.data.access_token;
+    grant_type: 'authorization_code',
+    client_id: REST_API_KEY,
+    redirect_uri: REDIRECT_URI,
+    code
+});
+const accessToken = tokenResponse.data.access_token;
         const userResponse = await axios.get('https://kapi.kakao.com/v2/user/me', {
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded",
